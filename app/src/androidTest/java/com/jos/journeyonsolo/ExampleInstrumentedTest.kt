e594d41ad757b5d51f25b3b614526b64d8326ee0
Naufalara <naufal.arya356@gmail.com>
2025-02-13 10:41:45 +0800

    change line 22 "rizky" to "jos"

--- app/src/androidTest/java/com/jos/journeyonsolo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jos/journeyonsolo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rizky.journeyonsolo", appContext.packageName)
+        assertEquals("com.jos.journeyonsolo", appContext.packageName)
     }
 }
